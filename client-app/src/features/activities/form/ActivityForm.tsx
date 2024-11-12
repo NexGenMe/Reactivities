cc758dc3a6696c15b3ab61132b08338d01934f7f
NexgenMe <harshg9795@gmail.com>
2024-11-12 20:08:09 +0530

    End of section 6

--- client-app/src/features/activities/form/ActivityForm.tsx
+++ client-app/src/features/activities/form/ActivityForm.tsx
@@ -12,12 +12,14 @@ interface Props {
   activity: Activity | undefined;
   closeForm: () => void;
   createOrEdit: (activity: Activity) => void;
+  submitting: boolean;
 }
 
 export default function ActivityForm({
   activity: selectedActivity,
   closeForm,
   createOrEdit,
+  submitting
 }: Props) {
   const initialState = selectedActivity ?? {
     id: "",
@@ -66,6 +68,7 @@ export default function ActivityForm({
         <FormInput
           placeholder="Date"
           value={activity.date}
+          type="date"
           name="date"
           onChange={handleInputChange}
         />
@@ -82,7 +85,7 @@ export default function ActivityForm({
           onChange={handleInputChange}
         />
 
-        <Button floated="right" positive type="submit" content="Submit" />
+        <Button loading={submitting} floated="right" positive type="submit" content="Submit" />
         <Button
           onClick={closeForm}
           floated="right"
